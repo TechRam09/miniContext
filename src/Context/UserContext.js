c9703e00519a08da5e9643ae526789d843fe461c
TechRam09 <nsshreeram@gmail.com>
2024-06-11 19:24:12 +0530

    Create UserContext.js
    Create a UserContext to store and access data

---
+++ src/Context/UserContext.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+//step1:create a context
+//create a provider
